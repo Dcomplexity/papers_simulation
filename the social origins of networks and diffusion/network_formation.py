6fcb57134b6332416e75a55eacfc9fde06680f59
Dcomplexity <dc199082@126.com>
2016-09-23 22:26:57 +0800

    201609232226

--- the social origins of networks and diffusion/network_formation.py
+++ the social origins of networks and diffusion/network_formation.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import math
 import networkx as nx
+import matplotlib.pyplot as plt
 
 dimension = []
 dimension.append({})
@@ -35,38 +36,38 @@ def find_position(now_position, distance):
 def consolidation_formation(dimension0, beta):  # beta represents consolidation
     constant_sum = 0
     for i in range(1, 7):
-        constant_sum = constant_sum + math.pow(2, (-beta) * i)
+        constant_sum = constant_sum + math.pow(math.e, (-beta) * i)
     constant = 1 / constant_sum
     probability = []
     for i in range(1, 7):
-        probability.append(constant * math.pow(2, (-beta) * i))
+        probability.append(constant * math.pow(math.e, (-beta) * i))
     probability_sum = []
     temp_sum = 0
     for i in range(len(probability)):
-        temp_sum = temp_sum + probability[i]  """account the probability, in order to find the max probability"""
+        temp_sum = temp_sum + probability[i]  #account the probability, in order to find the max probability
         probability_sum.append(temp_sum)
 
     individuals_0 = {}
     for keys in dimension0.keys():
         for items in dimension0[keys]:
-            individuals_0[items] = keys  """record the position(keys) of individuals(items) of dimension0"""
+            individuals_0[items] = keys  #record the position(keys) of individuals(items) of dimension0
 
-    individuals_con = {}     """individuals_con for individuals consolidation"""
+    individuals_con = {}     #individuals_con for individuals consolidation
     for keys in individuals_0.keys():
-        position_0 = individuals_0[keys]   """position_0 means the position of individual in dimension0"""
+        position_0 = individuals_0[keys]   #position_0 means the position of individual in dimension0
         p = random.uniform(0, 1)
         distance = 0
-        for i in range(len(probability_sum)):   """calculate the distances between different dimensions"""
+        for i in range(len(probability_sum)):   #calculate the distances between different dimensions
             if p < probability_sum[i]:
                 distance = i + 1
                 break
         position_new = find_position(position_0, distance)
-        individuals_con[keys] = position_new
+        individuals_con[keys] = position_new    #every individual gets a new position
 
     positions_con = {}
-    for i in range(32):
+    for i in range(32):    #32 positions in sum
         positions_con[i] = []
-    for keys in individuals_con.keys():
+    for keys in individuals_con.keys():    #keys means every individual
         positions_con[individuals_con[keys]].append(keys)
 
     return positions_con, individuals_con
@@ -74,22 +75,24 @@ def consolidation_formation(dimension0, beta):  # beta represents consolidation
 
 beta = 2
 alpha = 2
-for i in range(1, 10):
-    positions_con = consolidation_formation(dimension[0], beta)[0]
-    dimension.append(positions_con)
+for i in range(1, 10):     #10 dimensions
+    positions_con = consolidation_formation(dimension[0], beta)[0]   #positions_con is typed as a dictionary
+    dimension.append(positions_con)  #10 dictionaries
 print(len(dimension))
 individual_pos = []
 for i in range(len(dimension)):
+    print (dimension[i])
     individual_temp = {}
-    for keys in dimension[i]:
+    for keys in dimension[i].keys():
         for items in dimension[i][keys]:
             individual_temp[items] = keys
-    individual_pos.append(individual_temp)
+    individual_pos.append(individual_temp)   #ten dimensions, 3200 individuals, 32 positions, all record
 print(individual_pos)
+
 
 network = {}
 for i in range(3200):
-    network[i] = []
+    network[i] = []  #all 3200 individuals
 individual_group = range(3200)
 dimension_group = range(10)
 
@@ -97,11 +100,11 @@ dimension_group = range(10)
 def find_partner(now_individual, now_position, alpha, now_neighbor, dimension, selected_dimension, edge_number):
     constant_sum = 0
     for i in range(1, 7):
-        constant_sum = constant_sum + math.pow(2, (-alpha) * i)
+        constant_sum = constant_sum + math.pow(math.e, (-alpha) * i)
     constant = 1 / constant_sum
     probability = []
     for i in range(1, 7):
-        probability.append(constant * math.pow(2, (-alpha) * i))
+        probability.append(constant * math.pow(math.e, (-alpha) * i))
     probability_sum = []
     temp_sum = 0
     for i in range(len(probability)):
@@ -141,11 +144,11 @@ def find_partner(now_individual, now_position, alpha, now_neighbor, dimension, s
 total_edges = 16000
 now_edges = 0
 while now_edges < total_edges:
-    selected_individual = random.sample(individual_group, 1)[0]
-    selected_dimension = random.sample(dimension_group, 1)[0]
+    selected_individual = random.sample(individual_group, 1)[0]      #individual_group = range(3200)
+    selected_dimension = random.sample(dimension_group, 1)[0]        #dimension_group = range(10)
     selected_indiv_pos = individual_pos[selected_dimension][selected_individual]
     selected_partner, now_edges = find_partner(selected_individual, selected_indiv_pos, alpha, network[selected_individual], dimension, selected_dimension, now_edges)
-    network[selected_individual].append(selected_partner)
+    network[selected_individual].append(selected_partner)    #network = {}; for i in range(3200): network[i] = []
     network[selected_partner].append(selected_individual)
 
 edges_list= []
@@ -157,6 +160,28 @@ G.add_edges_from(edges_list)
 
 print (nx.is_connected(G))
 print (nx.average_clustering(G))
-print (nx.degree_histogram(G))
+print (G.degree())
+
+degree = nx.degree_histogram(G)          #返回图中所有节点的度分布序列
+x = range(len(degree))                             #生成x轴序列，从1到最大度
+y = [z / float(sum(degree)) for z in degree]
+#将频次转换为频率，这用到Python的一个小技巧：列表内涵，Python的确很方便：）
+plt.loglog(x,y,color="blue",linewidth=2)           #在双对数坐标轴上绘制度分布曲线
+plt.show()                                                          #显示图表
 
 nx.write_edgelist(G, "test.txt", data=False)
+
+RG = nx.random_graphs.random_regular_graph(10, 3200)
+
+print (nx.average_clustering(RG))
+
+dd = nx.average_clustering(G)
+ddd = nx.average_clustering(RG)
+print (dd / ddd)
+
+g_degree = G.degree()
+print (g_degree)
+total_degree = 0
+for keys in g_degree.keys():
+    total_degree += g_degree[keys]
+print (total_degree)
